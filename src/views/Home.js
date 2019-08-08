0b6f74caeba49e0481590e7ea60f2873c706a2a5
dengmengxin <dengmengxin@wondersgroup.com>
2019-08-08 11:23:34 +0800

    首页布局

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,6 +4,7 @@ class Home extends Component{
   render () {
     return(
       <div className="home">
+        {/* 页头公共区域，后面写成单独的组件 */}
         <header className="header">
         <div className="bx">
           <div className="title">
@@ -15,8 +16,18 @@ class Home extends Component{
           </div>
         </div>
         </header>
-        <div className="content bx">
+        {/* 日志的主要内容区域 */}
+          <div className="content bx">
+            <div className="left-info bs">
+              <div className="info-title">title</div>
+              <div className="journal">日志</div>
+            </div>
+            <div className="right-card">个人信息展示区</div>
         </div>
+        {/* 页脚公共区域，后面写成单独的组件 */}
+        <footer className="footer bx">
+          
+        </footer>
       </div>
     )
   }
